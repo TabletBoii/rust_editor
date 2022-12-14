67882cd07f9dc8cae4e3e53030a864cdced98808
TabletBoii <grand1354ru@gmail.com>
2022-12-14 23:56:36 +0600

    windows fix

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,27 @@
-use std::io::{self, stdout, Read};
-use termion::raw::IntoRawMode;
-fn main() {
-    let _stdout = stdout().into_raw_mode().unwrap();
+use std::io::{self, Read};
+use crossterm::terminal;
 
-    for b in io::stdin().bytes() {
-        let c = b.unwrap() as char;
-        println!("{}", c);
-        if c == 'q' {
+fn to_ctrl_byte(c: char) -> u8 {
+    let byte = c as u8;
+    byte & 0b0001_1111
+}
+
+fn die(err: std::io::Error){
+    panic!(e);
+}
+
+fn main() {
+    terminal::enable_raw_mode().unwrap();
+    for byte in io::stdin().bytes() {
+        let byte = byte.unwrap();
+        let character = byte as char;
+        if character.is_control() {
+            println!("{:?} \r", byte);
+        }
+        else {
+            println!("{:?} ({})\r", byte, character);
+        }
+        if b == to_ctrl_byte('q'){
             break;
         }
     }
